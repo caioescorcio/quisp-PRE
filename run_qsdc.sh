17077cbf30948596391761d863a4f9d6280b6191
caioescorcio <caioeldourado@gmail.com>
2026-06-26 15:56:44 +0200

    rename basic files

---
+++ run_qsdc.sh
@@ -0,0 +1,5 @@
+cd /home/caio/workspace/quisp-PRE
+./quisp/out/clang-release/quisp -u Qtenv -n ./quisp -f ./quisp/simulations/qsdc_network.ini --image-path=./quisp/images
+
+
+
--- run_quisp.sh
+++
@@ -1,5 +0,0 @@
-cd /home/caio/workspace/quisp-PRE
-./quisp/out/clang-release/quisp -u Qtenv -n ./quisp -f ./quisp/simulations/test_qsdc.ini --image-path=./quisp/images
-
-
-
